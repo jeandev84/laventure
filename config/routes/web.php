fb0875d28299f64b9fe5798e1050db655b637a6d
Jean Claude <jeanyao@ymail.com>
2024-01-23 16:19:27 +0500

    Add sub folder controllers and load it via attributes

--- config/routes/web.php
+++ config/routes/web.php
@@ -4,8 +4,11 @@ use App\Http\Controllers\WelcomeController;
 use Laventure\Component\Routing\Router\Router;
 
 return function (Router $router) {
+    /*
     $router->registerControllers([
        WelcomeController::class
     ]);
+    */
+
     return $router;
 };
